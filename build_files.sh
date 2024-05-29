5f0a82b970b9acbe6c44b84c35736e5bd5786086
cmvn-quangtran <quang.tran@classmethod.vn>
2024-05-29 16:23:09 +0700

    update code

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Building project package..."
-python -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo "Building migartion..."
 python manage.py makemigrations --noinput
