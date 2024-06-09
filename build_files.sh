b3872133b85ffdc3985d7bc85b8323c36c5300e0
cmvn-quangtran <quang.tran@classmethod.vn>
2024-06-09 17:18:35 +0700

    update cicd for project

--- build_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Building project package..."
-pip3 install -r requirements.txt
-
-echo "Building migartion..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-
